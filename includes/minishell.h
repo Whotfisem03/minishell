e421f2d9dc49382c80afed1f58bd55696ec0ff6b
Emma NGO <emma.tm.ngo@gmail.com>
2022-12-05 00:11:12 +0100

    start

---
+++ includes/minishell.h
@@ -0,0 +1,23 @@
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <stdio.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <sys/types.h>
+# include <sys/time.h>
+# include <sys/resource.h>
+# include <sys/wait.h>
+# include <signal.h>
+# include <unistd.h>
+# include <errno.h>
+# include <stdlib.h>
+# include <sys/stat.h>
+# include <dirent.h>
+# include <sys/ioctl.h>
+# include <termios.h>
+
+#define _POSIX_SOURCE
+
+
+#endif
